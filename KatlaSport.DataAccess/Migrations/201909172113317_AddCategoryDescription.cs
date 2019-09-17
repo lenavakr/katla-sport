894636b20c3e26975c4a187045724ede6787f2fb
Alena Kryvitskaya <krivitskaya.98@gmail.com>
2019-09-18 00:16:36 +0300

    Add Description property to ProductCategory model. Create a new migration AddCategoryDescription.

---
+++ KatlaSport.DataAccess/Migrations/201909172113317_AddCategoryDescription.cs
@@ -0,0 +1,15 @@
+using System.Data.Entity.Migrations;
+
+namespace KatlaSport.DataAccess.Migrations
+{
+    public partial class AddCategoryDescription : DbMigration
+    {
+        public override void Up()
+        {
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
