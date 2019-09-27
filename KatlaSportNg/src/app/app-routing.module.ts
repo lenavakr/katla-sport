a604ee2722c0eda697967371d1bca7dd2c0955b8
Alena Kryvitskaya <krivitskaya.98@gmail.com>
2019-09-28 00:53:26 +0300

    Merge step14. Add addHiveSection, updateHiveSection and deleteHiveSection methods to HiveSection service. Add fields to HiveSection model. Add markup for HiveSectionForm component. Implement HiveSectionForm component.

--- KatlaSportNg/src/app/app-routing.module.ts
+++ KatlaSportNg/src/app/app-routing.module.ts
@@ -25,7 +25,8 @@ const routes: Routes = [
   { path: 'hive', component: HiveFormComponent },
   { path: 'hive/:id', component: HiveFormComponent },
   { path: 'hive/:id/sections', component: HiveSectionListComponent },
-  { path: 'section/:id', component: HiveSectionFormComponent },
+  { path: 'section/:storeHiveId/:id', component: HiveSectionFormComponent },
+  { path: 'hive/:storeHiveId/section', component: HiveSectionFormComponent },
 ];
 
 @NgModule({
